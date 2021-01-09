ffacf649ed36fcd652537d810beb22a8f38e9932
David Lance <david6116@yahoo.com>
2021-01-09 01:38:01 -0500

    add gameSetup and colonize functions and some other initial definitions

---
+++ app.js
@@ -0,0 +1,29 @@
+
+
+let players = 0
+
+const playerColors = ['blue', 'red', 'yellow', 'white']
+const resources = ['ore', 'fuel', 'carbon', 'food', 'goods']
+const playersResources = {
+
+}
+
+const gameSetup = (numberOfPlayersSelected, colorsSelected) =>{
+    for (let i=0; i<numberOfPlayersSelected-1; i++){
+        playersResources[colorsSelected[i]] = []
+    }
+    console.log(playersResources)
+}
+//let's make sure the gameSetup works right
+gameSetup(4, ['red', 'yellow', 'white'])
+
+
+const colonize = (playerColor, newResources) => {
+    playersResources[playerColor].push(newResources)
+    console.log(playersResources)
+}
+//and make sure colonize works right
+colonize('red', 'ore')
+colonize('red', 'goods')
+
+console.log(playersResources)
